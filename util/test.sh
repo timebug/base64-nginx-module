d3e0fd9620d611812d753bfd2549993e2e9a9709
timebug <timebug.info@gmail.com>
2017-09-20 13:32:03 +0800

    bugfix: nginx binary path

--- util/test.sh
+++ util/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PATH=`pwd`/work/sbin:$PATH TEST_NGINX_NO_SHUFFLE=1 prove -I../test-nginx/lib -r t
+PATH=`pwd`/work/nginx/sbin:$PATH TEST_NGINX_NO_SHUFFLE=1 prove -I../test-nginx/lib -r t
